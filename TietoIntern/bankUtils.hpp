d6188eca24392f0bac24f6081cfc77e8f03cd0bb
kulavka <57875430+kulavka@users.noreply.github.com>
2022-05-29 19:30:14 +0300

    Add files via upload

---
+++ TietoIntern/bankUtils.hpp
@@ -0,0 +1,58 @@
+
+#ifndef BANKUTILS_HPP
+#define BANKUTILS_HPP
+
+#include "bankUtils.hpp"
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <string>
+using namespace std;
+
+
+
+
+class BankUtils {
+
+public:
+	 double interestRate;
+	BankUtils() {
+		// predefined interest rate
+		//interestRate = 10.0;
+	}
+
+	double ReadFromFile()	{
+		fstream myFile;
+		myFile.open("rate.txt", ios::in);
+		if (myFile.is_open()) {
+			string line;
+			while (getline(myFile, line)) {
+				return interestRate = 1/(atof(line.c_str()));
+			}
+			myFile.close();
+		}
+	}
+
+	// Method takes full amount and returns the value after the decimal point (cents)
+	int getCents(double amount) {
+		
+int amountC = fmod(amount * 100, 100);
+		return amountC;
+		
+		
+		
+	}
+
+	// Method takes full amount and returns the value before the decimal point (euros)
+	int getEuros(double amount) {
+		int amountE = trunc(amount);
+		return amountE;
+	}
+
+	// Method takes credit amount and credit period in months, applies the predefined interestRate
+	// and produces the total amount: creditAmount + interestRate over given months
+	double calculateInterestRate(double creditAmount, unsigned int months) {
+	}
+};
+#endif // !BANKUTILS_HPP
+#pragma once
---
+++ TietoIntern/main.cpp
@@ -0,0 +1,110 @@
+/*
+* TASK:
+* Develop a simple credit calculator which takes following values as user input:
+*   1) credit amount (type: double)
+*   2) credit period (in months)
+*
+* The calculator is expected to take the credit amount and apply the specified interest rate every month.
+* Every next month, the interest rate is applied to the total credit amount calculated for the previous month.
+* The interest rate is static and defined within the BankUtils class.
+* The calculator should implement and use BankUtils class for output setup and calculations.
+* The definition of class and corresponding methods can be found in bankUtils.hpp file.
+*
+* Bonus 1: implement logging to a file that tracks the applciation flow.
+*          Format and the information of the logs is not strictly defined - can be implemented as seen fit.
+*
+* Bonus 2: implement support for interest rate configuration by reading the value from a file.
+*          Format and the information of the configuration file is not strictly defined - can be implemented as seen fit.
+*
+* Bonus tasks are not mandatory, but an opportunity to showcase your knowledge and skills.
+*
+* OUTPUT:
+* The application is expected to return the following output:
+*   1) Total amount of credit and interest
+*   2) Total interest amount
+*
+* FORMAT:
+* Display the total amount of credit and interest in format: [TOTAL AMOUNT/INTEREST X EUROS AND Y CENTS]
+*
+* EXAMPLE:
+* User input:
+*   1) Credit amount: 100.00 EUR
+*   2) Credit period: 13 months
+* Interest rate (static): 10 %
+* Calculated result:
+*   Total credit and interest amount: 345.23
+*   Interest amount: 245.23
+* Output in console:
+*   'TOTAL AMOUNT 345 EUROS AND 23 CENTS'
+*   'TOTAL INTEREST 245 EUROS AND 23 CENTS'
+*
+* THINGS THAT MATTER:
+*   1) Code formatting
+*   2) Meaningful comments
+*   3) Handling of the edge cases
+*
+* NOTE: you can use any environemt/setup to implement the solution, but if you use any non-standard libraries, please, provide description of how to build and run the solution.
+*/
+
+#include "bankUtils.hpp"
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <string>
+using namespace std;
+
+int main() {
+
+	BankUtils bankUtils;
+
+	//Write time logs to file
+	fstream file;
+	file.open("logfile.txt", ios::app);
+	if (file.is_open()) {
+		time_t curr_time;
+		curr_time = time(NULL);
+		tm *tm_local = localtime(&curr_time);
+		file << "Logged time: " << tm_local->tm_hour << ":" << tm_local->tm_min << ":" << tm_local->tm_sec << endl;
+	file.close();
+	}
+	else
+	{
+		cout << "Problem with file opening";
+	}
+	
+	//Get percent rate from the file
+	//bankUtils.ReadFromFile();
+
+	double amount;
+	double interst;
+	int monthes;
+	cout << "Please, write credit amount: ";
+	cin >> amount;
+	cout << "Please, write monthes: ";
+	cin >> monthes;
+
+	double cred = round(amount * pow(1+bankUtils.ReadFromFile(),monthes)*100)/100;
+		
+	
+	interst = round((cred - amount)*100)/100;
+	
+	
+	//euros total amount
+	
+	cout << "TOTAL AMOUNT " << bankUtils.getEuros(cred) << " EUROS";
+
+//cents total amount
+	
+	cout << " AND " << bankUtils.getCents(cred) << " CENTS \n";
+
+	//euros Interest
+	
+	cout << "TOTAL INTEREST " << bankUtils.getEuros(interst) << " EUROS";
+
+	//cents Interest
+	
+	cout << " AND " << bankUtils.getCents(interst) << " CENTS \n";
+
+	system("pause");
+	return 0;
+}
